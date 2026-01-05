4d8c5e1b22f15ba8231496bda38396f92585b6b8
Jade Lassery <jadellassery@gmail.com>
2026-01-05 23:38:14 +0100

    Trying out astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
-import { defineConfig } from 'astro/config'
+// @ts-check
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
+// https://astro.build/config
 export default defineConfig({
-  site: 'https://jlassery.github.io',
-  base: '/jlassery.github.io',
-})
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
